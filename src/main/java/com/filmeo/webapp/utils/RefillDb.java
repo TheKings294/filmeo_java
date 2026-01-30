f8b29a054d7e9a870fbad6d89766e05baed0efb4
TheKings294 <98644094+TheKings294@users.noreply.github.com>
2026-01-30 16:15:35 +0100

    feat: add logo and image in fake data

--- src/main/java/com/filmeo/webapp/utils/RefillDb.java
+++ src/main/java/com/filmeo/webapp/utils/RefillDb.java
@@ -102,7 +102,8 @@ public class RefillDb {
             Human human = new Human();
             human.setFirstName(faker.name().firstName());
             human.setLastName(faker.name().lastName());
-            human.setProfilePicture(faker.internet().avatar());
+            human.setProfilePicture(i % 2 == 0 ? "https://media.themoviedb.org/t/p/w300_and_h450_face/nraZoTzwJQPHspAVsKfgl3RXKKa.jpg" :
+                    "https://media.themoviedb.org/t/p/w300_and_h450_face/o7sfT7EBtzxwiJiKmCafoebJQ2q.jpg");
             human.setGender(i % 2 == 0 ? GenderEnum.MALE : GenderEnum.FEMALE);
             Date birthDate = faker.date().birthday(20, 100);
             human.setBirthDate(convertToLocalDate(birthDate));
@@ -130,7 +131,8 @@ public class RefillDb {
             Movie movie = new Movie();
             movie.setTitle(faker.book().title());
             movie.setResume(faker.lorem().sentence(80));
-            movie.setPosterURL(faker.internet().avatar());
+            movie.setPosterURL(i % 2 == 0 ? "https://media.themoviedb.org/t/p/w300_and_h450_face/9ZmdDOIbiFCZOvRXBQ7muWUu32l.jpg" :
+                    "https://media.themoviedb.org/t/p/w300_and_h450_face/kQYx143yujkwyjJdZH2skOGeQfA.jpg");
             movie.setRealisator(humanRepository.findAll().get(getRandom(0, humanRepository.findAll().size() - 1)));
 
             for (int j = 0; j < getRandom(1, 10); j++) {
@@ -167,7 +169,8 @@ public class RefillDb {
             Seri seri = new Seri();
             seri.setTitle(faker.book().title());
             seri.setResume(faker.lorem().sentence(80));
-            seri.setPosterURL(faker.internet().avatar());
+            seri.setPosterURL(i % 2 == 0 ? "https://media.themoviedb.org/t/p/w300_and_h450_face/AoRfDyk5uTCtQn4O9Q8g5Bk2A1c.jpg"
+                    : "https://media.themoviedb.org/t/p/w300_and_h450_face/4Y5ZXYnWBIV8Vpe8hcA0LH6hC80.jpg");
             seri.setStaus(i % 2 == 0 ? SeriStatusEnum.FINISH : SeriStatusEnum.IN_PROGRESS);
             seri.setSeasons(getRandom(1, 8));
             seri.setEpisode(seri.getSeasons() * 20);
